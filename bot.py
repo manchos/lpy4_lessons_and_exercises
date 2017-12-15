2dde9f220e28b97db3e190dc35f28640e6b72cb9
manchos <vladm.ru@gmail.com>
2017-12-15 20:39:02 +0300

    experement sheduling message

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import re
+import schedule
+import time
 
 def greet_user(bot, update):
     print('Вызван /start')
@@ -36,18 +38,59 @@ def talk_to_me(bot, update):
             bot_response = "Лажа. В конце выражения должен стоять знак '='"
     except (AttributeError):
         bot_response = update.message.text
+        pass
     bot.sendMessage(update.message.chat_id, bot_response)
+
+
+
+
+# def shedule_message(bot, update):
+#     schedule.every(1).minutes.do(bot.sendMessage(update.message.chat_id, text='Давай общаться!'))
+#
+#
+# def callback_minute(bot, job):
+#     bot.send_message(job.context, text='One message every minute')
+
+
+
+def callback_alarm(bot, job):
+    print('callback_alarm')
+    bot.send_message(chat_id=job.context, text='BEEP')
+
+
+def callback_timer(bot, update, job_queue):
+    print('callback_timer')
+    bot.send_message(chat_id=update.message.chat_id, text='Setting a timer for 1 minute!')
+    job_queue.run_repeating(callback_alarm, 20, context=update.message.chat_id)
+
+#
+
+def callback_30(bot, job):
+    bot.send_message(chat_id='@manchos_bot', text='A single message with 30s delay')
 
 def main():
     updater = Updater("379170475:AAGMcg2qYxJwEUI9WkiUIkz-yD1UmfUESYI")
 
+    # j = updater.job_queue
+    #
+    # j.run_once(callback_30, 30, context=update.message.chat_id)
+
+
     dp = updater.dispatcher
+    dp.add_handler(CommandHandler('stench', callback_timer, pass_job_queue=True))
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("wordcount", words_count))
 
-    dp.add_handler(MessageHandler([Filters.text], talk_to_me))
+    dp.add_handler(CommandHandler("wordcount", words_count))
+
+    # dp.add_handler(MessageHandler([Filters.text], talk_to_me))
 
     dp.add_error_handler(show_error)
+
+
+
+
+
 
     updater.start_polling()
     updater.idle()
